3f4fff8e2d951632ed7c7da9eed7f7aeb1acb46b
noraloch <contact.nouraloudani@gmail.com>
2024-10-20 06:56:57 -0700

    finalized for sumbission

--- packages/nextjs/app/api/ipfs/add/route.ts
+++ packages/nextjs/app/api/ipfs/add/route.ts
@@ -2,7 +2,12 @@ import { ipfsClient } from "~~/utils/simpleNFT/ipfs";
 
 export async function POST(request: Request) {
   try {
+    console.log('request')
+    console.log(request.body)
     const body = await request.json();
+    console.log('body')
+    console.log(body)
+
     const res = await ipfsClient.add(JSON.stringify(body));
     return Response.json(res);
   } catch (error) {
